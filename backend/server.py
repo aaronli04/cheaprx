94b07d5f2ea651517334b3ad7d0ec763113c2bac
Aaron <aaronli5260@gmail.com>
2024-02-18 01:18:24 -0800

    scraping

--- backend/server.py
+++ backend/server.py
@@ -1,23 +1,28 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
-from utils import search_costplusdrugs
+from utils import search_costplusdrugs, search_blink_health
 
 app = Flask(__name__)
 CORS(app)
 
 @app.route('/api/medication', methods=['POST'])
 def get_data():
+    medications = []
+
     if request.method == 'POST':
         try:
             request_data = request.get_json()
             medication_name = request_data.get('name')
 
-            rows = search_costplusdrugs('./costplusdrugs/prescription_data.csv', medication_name)
+            costplusdrugs = search_costplusdrugs('./costplusdrugs/prescription_data.csv', medication_name)
+            medications = medications + costplusdrugs
 
-            print(rows)
-            result = {'message': 'Data received successfully', 'medication_name': medication_name}
+            blink_health = search_blink_health(medication_name)
+            medications.append(blink_health)
 
-            return jsonify(result)
+            result = {'medications': medications}
+
+            return jsonify(result), 200
         except Exception as e:
             return jsonify({'error': str(e)}), 400
 
--- backend/utils.py
+++ backend/utils.py
@@ -1,8 +1,25 @@
 import csv
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+    
+def extract_soup_with_selenium(link):
+    options = webdriver.ChromeOptions()
+    options.add_argument('--headless')
+    driver = webdriver.Chrome(options=options)
+    driver.get(link)
+    time.sleep(0.1)
+    page_source = driver.page_source
+    soup = BeautifulSoup(page_source, 'html.parser')
+    return soup
 
 # Search CostPlusDrugs for relevant drugs
 def search_costplusdrugs(file_path, name):
     matching_rows = []
+    medications = []
     name_lower = name.lower()
 
     with open(file_path, 'r', newline='') as csvfile:
@@ -12,8 +29,54 @@ def search_costplusdrugs(file_path, name):
             if name_lower in row['Name'].lower() or name_lower in row['Generic'].lower():
                 matching_rows.append(row)
 
-    return matching_rows
+    for row in matching_rows:
+        processed_medication = {
+            'name': row['Name'],
+            'generic': row['Generic'],
+            'link': row['Link'],
+            'price': row['Price'],
+            'prescription_needed': row['Prescription Needed'],
+            'strength': row['Strength'],
+            'count': row['Count']
+        }
+        medications.append(processed_medication)
 
-# Scrape GoodRx
-def search_goodrx(name):
-    url = 'https://www.goodrx.com/'
+    return medications
+
+# Scrape Blink Health
+def search_blink_health(name):
+    price = ''
+    strength = ''
+    count = ''
+    prescription_needed = False
+
+    link = 'https://www.blinkhealth.com/' + name
+    soup = extract_soup_with_selenium(link)
+
+    price_elem = soup.find('div', class_="text_Text_title4-blinkui__5v9KW text_interstate-regular__6jvdM text_interstate-regular__6jvdM text_Text_align-right__YLNqY")
+    price = price_elem.text.strip().replace('$', '').replace(',', '')
+
+    size_elem = soup.find('div', class_="input_input__fT6hC px-sm-16 py-sm-12")
+    parts = [part.strip() for part in size_elem.text.split(',')]
+    for part in parts:
+        if "mg" in part:
+            strength = part.lower()
+        elif any(char.isdigit() for char in part):
+            count = part.lower()
+
+    prescription_elem = soup.find('span', class_='text_Text_headline-blinkui__XtU_c text_interstate-regular__6jvdM text_interstate-regular__6jvdM')
+    prescription_status = prescription_elem.text.strip().lower()
+    if prescription_status == 'prescription required':
+        prescription_needed = True
+
+    medication = {
+        'name': name,
+        'generic': '',
+        'link': link,
+        'price': price,
+        'prescription_needed': prescription_needed,
+        'strength': strength,
+        'count': count
+    }
+
+    return medication
